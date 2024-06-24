f46487eacf9849cfc826242055d2bf0969ede43c
Irfan Ahmed <irfan@7ccgroup.com>
2024-06-24 03:25:35 -0500

    added server code

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 
+# Set the default primary key field type for the whole project
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
